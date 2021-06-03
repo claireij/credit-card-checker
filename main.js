d3852b845dfca691fd0388d0bd3911dd72a8b762
claireij <ruppelclaire@yahoo.de>
2021-06-03 09:53:38 +0200

    Changed the starting file

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-var express = require("express"); 
-var app = express(); 
-var path = require("path"); 
-app.get('/',function(req,res){ 
-  res.sendFile(path.join(__dirname+'/index.html')); 
-  //__dirname : It will resolve to your project folder. 
-}); 
-app.listen(3000);
-console.log("Server running at Port 3000"); 
-
 //For testing valid number: 4539677908016808
 
 //Get elements from HTML-Doc
