78a6aeab04d3c4cd9f998ab7c2c7ba36e591d4d9
claireij <ruppelclaire@yahoo.de>
2021-06-03 09:57:47 +0200

    Changed port

--- start.js
+++ start.js
@@ -5,5 +5,5 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html')); 
   //__dirname : It will resolve to your project folder. 
 }); 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Server running at Port 3000"); 
